8e8ff72be0cb5d49905f68316279092c34d27665
Ankit Kumar <ankitrbd6@gmail.com>
2023-02-03 22:36:41 +0530

    modifeied import(.js)

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import blogRouter from "./routes/blog-routes";
-import router from "./routes/user-routes";
+import router from "./routes/user-routes.js";
+import blogRouter from "./routes/blog-routes.js";
+
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -11,13 +12,14 @@ dotenv.config();
 app.use(cors()); //use cores before every middleware to disbles browsers cors privacy
 app.use(express.json()); 
 //to tell app to receive json data instead of body-parser
-app.get("/", (req, res) => {
-    res.send("Server is up and running");
-  });
+
 app.use("/api/user" ,router); //http://localhost:5000/api/user
 app.use("/api/blog", blogRouter); //upper router was for user router
+app.get("/", (req, res) => {
+  res.send("Server is up and running");
+});
 
 mongoose.connect(process.env.CONNECTION_URL)
-    .then(()=> app.listen(process.env.PORT||5000))
+    .then(()=> app.listen(process.env.PORT || 5000))
     .then(()=> console.log("Connected to DataBase and listening to 5000"))
     .catch((err) => console.log(err));
